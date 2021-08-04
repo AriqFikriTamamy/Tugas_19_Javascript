d96ca94fe6f1991b1858e1bb79d90c2f42a85fb4
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-08-04 20:57:24 +0700

    Tugas 19 Javascript

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+function repeat(){
+  var string = "Semangat...";
+  var bintang = "*"
+  console.log(string.repeat(5));
+  console.log(bintang.repeat(5));
+  console.log(bintang.repeat(5*5));
+  console.log(bintang.repeat(5*9));
+  console.log(bintang.repeat(5*13));
+  console.log(bintang.repeat(5*19));
+  console.log(bintang.repeat(5*13));
+  console.log(bintang.repeat(5*9));
+  console.log(bintang.repeat(5*5));
+  console.log(bintang.repeat(5))
+}
+
+repeat()
